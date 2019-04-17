00b299a7117c541928a5a7a32204f462e23863f8
boske <goran.bosic@openratio.com>
2019-04-17 16:18:15 +0200

    new add 12

--- resources/views/man.blade.php
+++ resources/views/man.blade.php
@@ -207,8 +207,8 @@
                     <div class="menu-container">
                         <div class="navbar-search-wrapper">
                             <div class="navbar-search vd_bg-black-30">
-                                <span class="append-icon"><i class="fa fa-search"></i></span>
-                                <input type="text" placeholder="Search" class="vd_menu-search-text no-bg no-bd vd_white width-70" name="search">
+
+
                                 <div class="pull-right search-config">
                                     <a data-toggle="dropdown" href="javascript:void(0);" class="dropdown-toggle"><span class="prepend-icon vd_grey"><i class="fa fa-cog"></i></span></a>
                                     <ul role="menu" class="dropdown-menu">
--- resources/views/offerMan/details.blade.php
+++ resources/views/offerMan/details.blade.php
@@ -152,7 +152,7 @@
                     <div class="menu-container">
                         <div class="navbar-search-wrapper">
                             <div class="navbar-search vd_bg-black-30">
-                                <span class="append-icon"><i class="fa fa-search"></i></span>
+
 
                                 <div class="pull-right search-config">
                                     <a data-toggle="dropdown" href="javascript:void(0);" class="dropdown-toggle"><span class="prepend-icon vd_grey"><i class="fa fa-cog"></i></span></a>
